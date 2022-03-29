6dcfd5f800ce84cf7249c1e99908645900dfcd13
lwb56 <wl56@rice.edu>
2022-03-29 09:51:59 -0500

    "added a utilites folder and a tracking.py function"

---
+++ utils/tracking.py
@@ -0,0 +1,16 @@
+import os, glob
+import numpy as np
+import scipy as sci
+import matplotlib.pyplot
+import pandas as pd
+
+
+def extract():
+    '''
+
+    '''
+
+    # first function
+
+    infliles  = glob.glob(os.path,join())
+
