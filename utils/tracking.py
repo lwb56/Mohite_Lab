ed42815fb6f7e887f5feb6b8239b1abbafa082a4
lwb56 <wl56@rice.edu>
2022-03-29 13:50:07 -0500

    added functions

--- utils/tracking.py
+++ utils/tracking.py
@@ -1,16 +1,31 @@
 import os, glob
 import numpy as np
 import scipy as sci
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def extract():
+def extract(filename):
+    '''
+    This function extracts the the q and intensity values from the filename and outputs a matrix of (n,2) Q vs I
     '''
 
-    '''
+    # open files for extraction 
+    f = open(filename, 'r')
 
-    # first function
+    q = []
+    I = []
+    for i in f.readlines():
+        dat =i.split(' ')
+        if len(dat) > 4:
+            continue
 
-    infliles  = glob.glob(os.path,join())
+        q.append(float(dat[0]))
+        I.append(float(dat[2]))
 
+    q = np.array(q)
+    I = np.array(I)
+
+    data = np.concatenate((q.reshape(-1,1),I.reshape(-1,1)), axis = 1)
+
+    return data
